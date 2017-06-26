1d9a0e82888adc21d932ccbccbec5d6e71eabba4
redroboto <vince.pleyto@gmail.com>
2017-06-26 23:51:40 +0800

    added database

--- index.php
+++ index.php
@@ -1,26 +1,38 @@
 <?php
 
-class Post {
-	private $name;
+require 'classes/database.php';
 
-	public function __set($name,$value){
-		echo 'Setting '.$name.' to <strong>'.$value.'</strong><br>';
-		$this->name = $value;
+$database = new Database;
+
+$database->query('SELECT * FROM myblog.posts WHERE id = :id');
+$database->bind(':id','1');
+$rows = $database->resultset();
+
+$post = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+if($post['submit']){
+	$title = $post['title'];
+	$body = $post['body'];
+	echo $title;
 	}
-
-	public function __get($name){
-		echo 'Getting '.$name.' <strong>'.$this->name.'</strong><br>';
-	}
-
-	public function __isset($name){
-		echo 'Is '.$name.' set? <br>';
-		return isset($this->name);
-	}
-}
-
-$post = new Post;
-$post->name = 'Hello';
-echo $post->name;
-var_dump(isset($post->name));
-
 ?>
+<h1>Add Post</h1>
+<form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
+	<label>Post Title</label><br>
+	<input type="text" name="title" placeholder="Add a title"><br>
+	<label>Post Body</label><br>
+	<textarea name="body"></textarea><br>
+	<input type="submit" name="submit" value="Submit">
+</form>
+
+<h1>Posts</h1>
+<div>
+	<?php foreach($rows as $row) : ?>
+
+	<div>
+		<h3><?php echo $row['title']; ?></h3>
+		<p><?php echo $row['body']; ?></p>
+	</div>
+
+	<?php endforeach; ?>
+</div>
