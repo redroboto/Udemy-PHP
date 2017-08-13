3376fa3e1226bfee30b12c03c037749b7426fb77
redroboto <vince.pleyto@gmail.com>
2017-08-13 15:00:30 +0800

    changed add to Update, added delete, added isset function

--- index.php
+++ index.php
@@ -9,28 +9,35 @@ $database = new Database;
 //use this to filter special characters from the string input (to prevent injection of code)
 $post = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
-if($post['submit']){
+if(isset($_POST['delete'])){
+	$delete_id =  $_POST['delete_id'];
+	$database->query('DELETE FROM posts WHERE id = :id');
+	$database->bind(':id', $delete_id);
+	$database->execute();
+}
+
+if(isset($post['submit'])){
+	$id = $post['id'];
 	$title = $post['title'];
 	$body = $post['body'];
 	
-	$database->query('INSERT INTO posts (title, body) VALUES (:title, :body)');
+	$database->query('UPDATE posts SET title = :title, body = :body WHERE id = :id');
 	$database->bind(':title', $title);
 	$database->bind(':body', $body);
+	$database->bind(':id', $id);
 	$database->execute();
-
-	if($database->lastInsertId()){
-		echo '<p>Post Added!</p>';
-		}
-	}
+}
 
 $database->query('SELECT * FROM myblog.posts');
 // $database->bind(':id','1');
 $rows = $database->resultset();
 
 ?>
-<h1>Add Post</h1>
+<h1>Edit Post</h1>
 <!-- PHP Global to redirect to the same page -->
 <form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
+	<label>Post ID</label><br>
+	<input type="text" name="id" placeholder="Specify ID"><br>
 	<label>Post Title</label><br>
 	<input type="text" name="title" placeholder="Add a title"><br>
 	<label>Post Body</label><br>
@@ -43,8 +50,12 @@ $rows = $database->resultset();
 	<?php foreach($rows as $row) : ?>
 
 	<div>
-		<h3><?php echo $row['title']; ?></h3>
+		<h3><?php echo $row['title']." ID ".$row['id']; ?></h3>
 		<p><?php echo $row['body']; ?></p>
+		<form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
+			<input type="hidden" name="delete_id" value="<?php echo $row['id']; ?>">
+			<input type="submit" name="delete" value="Delete">
+		</form>
 	</div>
 
 	<?php endforeach; ?>
